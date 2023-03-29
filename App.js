6bceb0212d3ee4b318feae09e6c4019c58b4a114
xCrina <crina.popovici@student.upt.ro>
2023-03-29 18:00:31 +0300

    Initial commit

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Button, StyleSheet, Text, View} from 'react-native';
 
 export default function App() {
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app!</Text>
+      <Button title="ON"></Button>
+      <Button title="OFF"></Button>
       <StatusBar style="auto" />
     </View>
   );
