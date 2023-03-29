80ec29139dec638fc503a0e3342719bd224e9d6f
xCrina <crina.popovici@student.upt.ro>
2023-03-29 20:28:32 +0300

    Adding background image

--- App.js
+++ App.js
@@ -1,17 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import {Button, StyleSheet, Text, View} from 'react-native';
+import {ImageBackground, Button, StyleSheet, Text, View, ImageBackgroundComponent} from 'react-native';
+
+const image = require("./bee.jpg");
 
 export default function App() {
 
   return (
-    <View style={styles.container}>
-      <Text>Hello</Text>
-      <Button title="ON"></Button>
-      <Button title="OFF"></Button>
+    <ImageBackground source={image} style={styles.container}>
+      <Text>Welcome to our app!</Text>
+      <Button title="ON" color={'#808000'}></Button>
+      <Button title="OFF" color={'#808000'}></Button>
       <Text>Hello!</Text>
 
         <StatusBar style="auto" />
-    </View>
+    </ImageBackground>
   );
 }
 
@@ -19,7 +21,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    image: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%',
+        height: '100%',
+      },
     alignItems: 'center',
     justifyContent: 'center',
   },
+    headerImage: {
+        width: 415,
+        height: 940,
+    }
 });
