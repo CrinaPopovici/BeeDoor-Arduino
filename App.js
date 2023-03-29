c3cdbe63fd6a574eaad317d0d1180a8373e61645
xCrina <crina.popovici@student.upt.ro>
2023-03-29 18:08:11 +0300

    Test

--- App.js
+++ App.js
@@ -5,11 +5,12 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello</Text>
       <Button title="ON"></Button>
       <Button title="OFF"></Button>
-      <StatusBar style="auto" />
+      <Text>Hello!</Text>
+
+        <StatusBar style="auto" />
     </View>
   );
 }
