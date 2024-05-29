9d74697b4dffdb8a49fb87b23d53c80ab7ca370f
CrinaPopovici <crina_ileana@yahoo.com>
2024-05-29 17:14:11 +0300

    Implemented useEffect to read temperature and humidity in real time

--- Frontend/navigation/screens/HomeScreen.js
+++ Frontend/navigation/screens/HomeScreen.js
@@ -1,18 +1,59 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ImageBackground, StyleSheet, View, Text } from "react-native";
 import { StatusBar } from "expo-status-bar";
+import { Button } from "react-native-paper";
 
 const image = require("../bee.jpg");
 
-export default function HomeScreen({ navigation, temperature, humidity }) {
+export default function HomeScreen({ navigation }) {
+  const [temperature, setTemperature] = useState(null);
+  const [humidity, setHumidity] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const tempResponse = await fetch("http://192.168.3.218/temperature");
+        const tempData = await tempResponse.text();
+        setTemperature(tempData);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+   fetchData();
+    const interval = setInterval(fetchData, 5000); // Fetch data every 2 seconds
+    return () => clearInterval(interval, interval1);
+  }, []);
+
+  useEffect(()=>{
+    //Humidity
+    const fetchHumidity = async () => {
+      try {
+        const humResponse = await fetch("http://192.168.3.218/humidity");
+        const humData = await humResponse.text();
+        setHumidity(humData);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchHumidity();
+    const interval = setInterval(fetchHumidity, 10000); // Fetch data every 2 seconds
+    return () => clearInterval(interval, interval1);
+  },[])
+  console.log(humidity);
+  console.log(temperature);
   return (
     <ImageBackground source={image} style={styles.container}>
       <View style={styles.contentContainer}>
         <View style={styles.textContainer}>
-          <Text style={styles.value}>Humidity: {humidity}</Text>
+          <Text style={styles.label}>
+            Humidity: {humidity ? `${humidity} %` : "Loading..."}
+          </Text>
         </View>
         <View style={styles.textContainer}>
-          <Text style={styles.value}>Temperature: {temperature}</Text>
+          <Text style={styles.label}>
+            Temperature: {temperature ? `${temperature} °C` : "Loading..."}
+          </Text>
         </View>
       </View>
 
