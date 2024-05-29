6269540918def2d1e94ba8c859e7422ed52ed505
CrinaPopovici <crina_ileana@yahoo.com>
2024-05-29 20:56:34 +0300

    update  .gitignore file and separated backend into three layer arhitecture

--- Frontend/navigation/screens/PowerScreen.js
+++ Frontend/navigation/screens/PowerScreen.js
@@ -1,7 +1,13 @@
-import React, {useState} from 'react';
-import {View, Text, TouchableOpacity, StyleSheet, Dimensions} from 'react-native';
-import NetInfo from '@react-native-community/netinfo';
-import axios from 'axios';
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  Dimensions,
+} from "react-native";
+import NetInfo from "@react-native-community/netinfo";
+import axios from "axios";
 
 // const SERVER_IP = '172.20.10.10'; // Adresa IP a Raspberry Pi care rulează serverul
 // const SERVER_PORT = 3002; // Portul serverului
@@ -22,87 +28,90 @@ import axios from 'axios';
 //     });
 // };
 
-const ESP8266_IP = "http://192.168.3.218"; 
+const ESP8266_IP = "http://192.168.3.218";
 
 export const turnOn = async () => {
-    try {
-      await fetch(`${ESP8266_IP}/on`);
-    } catch (error) {
-      Alert.alert("Eroare la aprinderea becului");
-    }
-  };
+  try {
+    await fetch(`${ESP8266_IP}/openDoor`);
+  } catch (error) {
+    Alert.alert("Eroare la aprinderea becului");
+  }
+};
 
- export const turnOff = async () => {
-    try {
-      await fetch(`${ESP8266_IP}/off`);
-    } catch (error) {
-      Alert.alert("Eroare la stingerea becului");
-    }
-  };
-
-
+export const turnOff = async () => {
+  try {
+    await fetch(`${ESP8266_IP}/closeDoor`);
+  } catch (error) {
+    Alert.alert("Eroare la stingerea becului");
+  }
+};
 
 const PowerScreen = () => {
-    const [motorOnSalcam, setMotorOnSalcam] = useState(false);
-    const [motorOnFloareaSoarelui, setMotorOnFloareaSoarelui] = useState(false);
+  const [motorOnSalcam, setMotorOnSalcam] = useState(false);
+  const [motorOnFloareaSoarelui, setMotorOnFloareaSoarelui] = useState(false);
 
-    // const handleMotorSalcamToggle = () => {
-    //     const command = motorOnSalcam ? 'STOPSALCAM' : 'STARTSALCAM';
-    //     console.log(`Stare curentă a motorului: ${motorOnSalcam}`);
-    //     console.log(`Comandă trimisă: ${command}`);
-    //     sendCommand(command);
-    //     setMotorOnSalcam(!motorOnSalcam);
-    // };
-    // const handleMotorFloareaSoareluiToggle = () => {
-    //     const command = motorOnSalcam ? 'STOPFLOAREASOARELUI' : 'STARTFLOAREASOARELUI';
-    //     console.log(`Stare curentă a motorului: ${motorOnFloareaSoarelui}`);
-    //     console.log(`Comandă trimisă: ${command}`);
-    //     sendCommand(command);
-    //     setMotorOnFloareaSoarelui(!motorOnFloareaSoarelui);
-    // };
+  // const handleMotorSalcamToggle = () => {
+  //     const command = motorOnSalcam ? 'STOPSALCAM' : 'STARTSALCAM';
+  //     console.log(`Stare curentă a motorului: ${motorOnSalcam}`);
+  //     console.log(`Comandă trimisă: ${command}`);
+  //     sendCommand(command);
+  //     setMotorOnSalcam(!motorOnSalcam);
+  // };
+  // const handleMotorFloareaSoareluiToggle = () => {
+  //     const command = motorOnSalcam ? 'STOPFLOAREASOARELUI' : 'STARTFLOAREASOARELUI';
+  //     console.log(`Stare curentă a motorului: ${motorOnFloareaSoarelui}`);
+  //     console.log(`Comandă trimisă: ${command}`);
+  //     sendCommand(command);
+  //     setMotorOnFloareaSoarelui(!motorOnFloareaSoarelui);
+  // };
 
-    return (
-        <View style={styles.container}>
-            <TouchableOpacity style={styles.button} onPress={turnOn}>
-                <Text
-                    style={styles.text}>{motorOnSalcam ? 'Oprire culegere la salcam' : 'Pornire culegere la salcam'}</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.button2} onPress={turnOff}>
-                <Text
-                    style={styles.text}>{motorOnFloareaSoarelui ? 'Oprire culegere la floarea soarelui' : 'Pornire culegere la floarea soarelui'}</Text>
-            </TouchableOpacity>
-
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.button} onPress={turnOn}>
+        <Text style={styles.text}>
+          {motorOnSalcam
+            ? "Oprire culegere la salcam"
+            : "Pornire culegere la salcam"}
+        </Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button2} onPress={turnOff}>
+        <Text style={styles.text}>
+          {motorOnFloareaSoarelui
+            ? "Oprire culegere la floarea soarelui"
+            : "Pornire culegere la floarea soarelui"}
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
 };
 const styles = StyleSheet.create({
-    container: {
-        alignItems: 'stretch',
-        paddingTop: 5,
-        paddingBottom: 5,
-        marginBottom: Dimensions.get('window').height * 0.4, // 40% din inaltimea ecranului
-    },
-    button: {
-        width: '100%',
-        height: 60, // înălțime fixă
-        backgroundColor: '#010013',
-        justifyContent: 'center',
-        alignItems: 'center',
-        //borderRadius: 5,
-    },
-    button2: {
-        width: '100%',
-        height: 60, // înălțime fixă
-        backgroundColor: '#e3e200',
-        justifyContent: 'center',
-        alignItems: 'center',
-        //borderRadius: 5,
-    },
+  container: {
+    alignItems: "stretch",
+    paddingTop: 5,
+    paddingBottom: 5,
+    marginBottom: Dimensions.get("window").height * 0.4, // 40% din inaltimea ecranului
+  },
+  button: {
+    width: "100%",
+    height: 60, // înălțime fixă
+    backgroundColor: "#010013",
+    justifyContent: "center",
+    alignItems: "center",
+    //borderRadius: 5,
+  },
+  button2: {
+    width: "100%",
+    height: 60, // înălțime fixă
+    backgroundColor: "#e3e200",
+    justifyContent: "center",
+    alignItems: "center",
+    //borderRadius: 5,
+  },
 
-    text: {
-        color: 'white',
-        fontSize: 18,
-    },
+  text: {
+    color: "white",
+    fontSize: 18,
+  },
 });
 
 export default PowerScreen;
